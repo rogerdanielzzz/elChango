cdf043a819ed9fbebbebf6a39b7efe30edfc2149
Roger Perez <roger.perezcol@gmail.com>
2022-03-14 21:03:38 -0300

    Toastify added

--- main.js
+++ main.js
@@ -30,6 +30,8 @@ let listaCarrefour = []
 if (datosCarrefour) {
     listaCarrefour = [...datosCarrefour]
 }
+// Array donde se filtra con los objetos con menor precio cuando encuentran una coincidencia de nombre
+let filtrado = [];
 
 
 //Variables  para poder modificar dom
@@ -41,6 +43,10 @@ let formProducto = document.getElementById('formProducto')
 let botonProductos = document.getElementById('botonProductos')
 let botonEliminarCoto = document.getElementById('botonEliminarCoto')
 let botonEliminarCarrefour = document.getElementById('botonEliminarCarrefour')
+let supermercado = document.getElementById('supermercado')
+let comunidad = document.getElementById('comunidad')
+
+
 
 // Evento de submit 
 formProducto.addEventListener('submit', (e) => {
@@ -48,37 +54,57 @@ formProducto.addEventListener('submit', (e) => {
 
     let nombre = document.getElementById('nombre').value
     let categoria = document.getElementById('categoria').value
-    let supermercado = document.getElementById('supermercado').value
+    supermercado = document.getElementById('supermercado').value
     let precio = document.getElementById('precio').value
     // se parsea el valor de entrada ya que a pesar que es input type number JS lo detecta como string
     precio = parseInt(precio)
-    let comunidad = document.getElementById('comunidad').value
+    comunidad = document.getElementById('comunidad').value
 
     // Se crea el objeto con los valores de los inputs
     let producto = new Productos(nombre, categoria, supermercado, precio, comunidad)
 
     // se Aplica 3 condicionales para saber en que array agregar el objeto y si en caso de ser de coto le corresponde el descuento comunidad a ese producto
     if ((supermercado == "Coto") && (comunidad == "No")) {
-        comunidad = false;
+        producto.comunidad = false;
+
         listaCoto.push(producto);
         localStorage.setItem('productosCoto', JSON.stringify(listaCoto))
 
     } else if ((supermercado == "Coto") && (comunidad == "Si")) {
-        comunidad = true;
+        producto.comunidad = true;
         producto.descuentoComunidad()
         listaCoto.push(producto);
         localStorage.setItem('productosCoto', JSON.stringify(listaCoto))
 
     } else if (supermercado == "Carrefour") {
-        comunidad = false;
+        producto.comunidad = false;
         listaCarrefour.push(producto);
         localStorage.setItem('productosCarrefour', JSON.stringify(listaCarrefour))
 
     }
 
+    Toastify({
+        text: "Producto Agregado",
+        duration: 3000,
+        className: "toastNotify",
+        style: {
+            color: "#000000",
+            background: "#ffc107",
+
+        },
+
+        offset: {
+            y: 55
+        },
+
+    }).showToast();
 
     // Se borra los input al terminar el evento asi facilitando el proceso de agregar mas objetos
     formProducto.reset()
+})
+
+supermercado.addEventListener('input', (e) => {
+    comunidad.disable = true;
 })
 
 
@@ -91,8 +117,8 @@ botonProductos.addEventListener('click', () => {
         divListaCoto.innerHTML = ""
         totalCoto.innerHTML = ``
         botonEliminarCoto.innerHTML = ``
-       
-       
+
+
         listaCoto.forEach((productos, index) => {
             divListaCoto.innerHTML += `
                 <div class="card mx-auto my-5 text-dark bg-warning" id="producto${index}" style="width: 18rem;"> 
@@ -122,12 +148,35 @@ botonProductos.addEventListener('click', () => {
 
 // Se procede a las mismos eventos y funciones del array de coto
 botonProductos.addEventListener('click', () => {
+
+    // Se aplica un bucle para poder comparar los 2 arrays y tener como resultado otro array con los precios mas bajos
+    for (var i = 0; i < listaCoto.length; i++) {
+
+        for (var j = 0; j < listaCarrefour.length; j++) {
+            if (listaCoto[i]['nombre'] == listaCarrefour[j]['nombre']) {
+
+
+                // Operador Ternario if else 
+                listaCoto[i]["precio"] > listaCarrefour[j]["precio"] ? filtrado.push(listaCarrefour[j]) : filtrado.push(listaCoto[i]);
+
+            }
+        }
+
+    }
+
+
+    console.log(filtrado)
+
+
+    // Se agregan bucles con condiciones en el cual crea una carda con cada objeto del Array 
+
     if (listaCarrefour.length !== 0) {
         divListaCarrefour.innerHTML = ""
         totalCarrefour.innerHTML = ``
         botonEliminarCarrefour.innerHTML = ``
-      
-      
+
+
+
         listaCarrefour.forEach((productos, index) => {
             divListaCarrefour.innerHTML += `
                 <div class="card mx-auto my-5 text-dark bg-warning" id="producto${index}" style="width: 18rem;"> 
@@ -171,7 +220,7 @@ botonEliminarCoto.addEventListener('click', () => {
 
 botonEliminarCarrefour.addEventListener('click', () => {
 
-    divListaCarrefour.innerHTML = "No se agrego ningun producto de Coto"
+    divListaCarrefour.innerHTML = "No se agrego ningun producto de Carrefour"
     totalCarrefour.innerHTML = ``
     botonEliminarCarrefour.innerHTML = ``
 
